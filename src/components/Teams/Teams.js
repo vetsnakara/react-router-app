3dabc66e6b7650332fdf4411866ecae8c7821fdc
Konstantin Arakantsev <kostya3312@gmail.com>
2020-03-28 16:50:54 +0300

    feat: fetching the teams

--- src/components/Teams/Teams.js
+++ src/components/Teams/Teams.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+// import { Route } from 'react-router-dom'
+// import { parse } from 'query-string'
+// import slug from 'slug'
 
-const Teams = () => {
+import { getTeamNames } from '../../api'
+
+import Sidebar from '../Sidebar'
+
+const Teams = ({ location, match }) => {
+  const [state, setState] = useState({
+    teams: [],
+    loading: true
+  })
+
+  const fetchTeams = teamId => {
+    getTeamNames(teamId).then(teamNames => {
+      setState({
+        teams: teamNames,
+        loading: false
+      })
+    })
+  }
+
+  useEffect(() => {
+    fetchTeams()
+  }, [])
+
+  const { loading, teams } = state
+
   return (
     <div className='container two-column'>
-      Teams
+      <Sidebar
+        title='Teams'
+        loading={loading}
+        list={teams}
+      />
+
+      {
+        !loading && location.pathname === '/teams'
+          ? <div className='sidebar-instruction'>Select a team</div>
+          : null
+      }
     </div>
   )
 }
